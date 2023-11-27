05660763e2d1a7b6836dcecf13cad924df31c01b
cherryApp <j.cserko@gmail.com>
2023-11-27 08:37:20 +0100

    fix dev url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: () => `${window.location.protocol}//${window.location.host}/api/`
+    apiUrl: () => `${window.location.protocol}//${window.location.host}/api/`,
 };
